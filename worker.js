729fdc0d0a7814a3473cdb0297c75d6fe51ea7c8
modii1 <zerof443@gmail.com>
2025-11-30 16:41:55 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,12 @@
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    // First, try to serve static asset
-    const out = await env.ASSETS.fetch(request);
-    if (out.status !== 404) {
-      return out;
-    }
-    // If not asset, return index.html (for SPA routing)
-    const res = await env.ASSETS.fetch(new Request(new URL("index.html", request.url).toString(), request));
-    return new Response(await res.text(), {
-      headers: { "Content-Type": "text/html" }
+  async fetch(request) {
+    // يخدم ملفات مجلد public (الذي بداخله index.html)
+    return serveDir(request, {
+      fsRoot: "public",
+      showDirListing: false,
+      quiet: true,
     });
   }
 };
